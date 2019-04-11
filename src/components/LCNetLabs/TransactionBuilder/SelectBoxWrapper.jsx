510212acae86d2a1c4499c1763b757c17b5ea66b
amitchandravanshi <mail.amitchandravanshi@gmail.com>
2019-04-11 15:17:11 +0530

    responsive dropdown

--- src/components/LCNetLabs/TransactionBuilder/SelectBoxWrapper.jsx
+++ src/components/LCNetLabs/TransactionBuilder/SelectBoxWrapper.jsx
@@ -1,12 +1,16 @@
 import styled, { css } from 'styled-components';
-import { utils } from "styled-minimal";
+import { utils } from 'styled-minimal';
 
-const {responsive} = utils;
+const { responsive } = utils;
 
 const SelectBoxWrapper = styled.div`
   width: 350px;
   position: absolute;
   top: 15px;
+
+  @media (max-width: 400px) {
+    width: 256px;
+  }
 
   ${props =>
     props.top &&
